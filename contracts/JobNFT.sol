c41ff62bfde1aa6f1bad1fc699a0a6fafae82c34
Gareth Edwards <garrethedwards@gmail.com>
2022-09-07 21:18:31 +0200

    🚀⛓ Initial Commit ⛓🚀

---
+++ contracts/JobNFT.sol
@@ -0,0 +1,96 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.2;
+
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/utils/Counters.sol";
+
+contract JobNFT is
+ERC721,
+ERC721URIStorage,
+Ownable
+{
+    using Counters for Counters.Counter;
+
+    Counters.Counter private _tokenIdCounter;
+    mapping(address => MintApproval) employeeToApproval;
+
+    struct MintApproval {
+        string uri;
+        address employer;
+    }
+
+    constructor() ERC721("Proof Of Position", "POPP") {}
+
+    /**
+     * @dev Create approval for an employee to mint a POPP
+     */
+    function approveMint(
+        address employee,
+        string memory uri
+    ) public {
+        MintApproval memory approval = MintApproval(
+            uri,
+            _msgSender()
+        );
+        employeeToApproval[employee] = approval;
+    }
+
+    function mintItem(address employee) public {
+        MintApproval memory approval = getApproval(employee);
+        require(approval.employer != address(0), "you don't have approval to mint this NFT");
+        _tokenIdCounter.increment();
+        uint256 tokenId = _tokenIdCounter.current();
+        _safeMint(employee, tokenId);
+        _setTokenURI(tokenId, approval.uri);
+        delete employeeToApproval[_msgSender()];
+    }
+
+    function getApproval(address employee) public view returns (MintApproval memory) {
+        return employeeToApproval[employee];
+    }
+
+    function burn(uint256 tokenId) external {
+        require(
+            _msgSender() == ownerOf(tokenId)
+            || owner() == _msgSender(),
+                "Only the owner can do this"
+        );
+        _burn(tokenId);
+    }
+    // The following functions are overrides required by Solidity.
+
+    function _beforeTokenTransfer(
+        address from,
+        address to,
+        uint256 tokenId
+    ) internal pure override(ERC721) {
+        require(from == address(0) || to == address(0), "POPP is non-transferable");
+    }
+
+    function _burn(uint256 tokenId)
+    internal
+    override(ERC721, ERC721URIStorage)
+    {
+        super._burn(tokenId);
+    }
+
+    function tokenURI(uint256 tokenId)
+    public
+    view
+    override(ERC721, ERC721URIStorage)
+    returns (string memory)
+    {
+        return super.tokenURI(tokenId);
+    }
+
+    function supportsInterface(bytes4 interfaceId)
+    public
+    view
+    override(ERC721)
+    returns (bool)
+    {
+        return super.supportsInterface(interfaceId);
+    }
+}
---
+++ test/job_nft_test.js
@@ -0,0 +1,78 @@
+//
+// this script executes when you run 'yarn test'
+//
+// you can also test remote submissions like:
+// CONTRACT_ADDRESS=0x43Ab1FCd430C1f20270C2470f857f7a006117bbb yarn test --network rinkeby
+//
+// you can even run mint commands if the tests pass like:
+// yarn test && echo "PASSED" || echo "FAILED"
+//
+const {expect} = require("chai");
+
+describe("🚩 Job NFT User Flows", function () {
+    this.timeout(120000);
+
+    let myContract;
+    let owner;
+    let alice;
+    let bob;
+
+    // console.log("hre:",Object.keys(hre)) // <-- you can access the hardhat runtime env here
+
+    describe("JobNFT", function () {
+        // `beforeEach` will run before each test, re-deploying the contract every
+        // time. It receives a callback, which can be async.
+        beforeEach(async function () {
+            const Popp = await ethers.getContractFactory("JobNFT");
+            myContract = await Popp.deploy();
+
+            [owner, alice, bob] = await ethers.getSigners();
+        });
+
+        describe("approve()", function () {
+            it("Should be able to approve an employee to mint", async function () {
+                await myContract.approveMint(
+                    alice.address,
+                    "QmfVMAmNM1kDEBYrC2TPzQDoCRFH6F5tE1e9Mr4FkkR5Xr"
+                );
+
+                await myContract.connect(alice).mintItem(alice.address);
+                const aliceBalance = await myContract.balanceOf(alice.address);
+                expect(aliceBalance.toBigInt()).to.equal(1);
+
+                await expect(
+                    myContract.connect(alice).transferFrom(alice.address, bob.address, 1)
+                ).to.be.revertedWith("POPP is non-transferable");
+            });
+
+            it("Should not be able to mint without approval", async function () {
+                // test no approval
+                await expect(
+                    myContract.connect(bob).mintItem(bob.address)
+                ).to.be.revertedWith("you don't have approval to mint this NFT");
+                const bobBalance = await myContract.balanceOf(bob.address);
+                expect(bobBalance.toBigInt()).to.equal(0);
+            });
+        });
+
+        describe("burn()", function () {
+            it("Should be able to burn your popp", async function () {
+                await myContract.approveMint(
+                    alice.address,
+                    "QmfVMAmNM1kDEBYrC2TPzQDoCRFH6F5tE1e9Mr4FkkR5Xr"
+                );
+
+                await myContract.connect(alice).mintItem(alice.address);
+
+                await expect(
+                    myContract.connect(bob).burn(1)
+                ).to.be.revertedWith("Only the owner can do this");
+
+                await myContract.connect(alice).burn(1);
+                const aliceBalance = await myContract.balanceOf(alice.address);
+                expect(aliceBalance.toBigInt()).to.equal(0);
+            });
+        });
+
+    });
+});
