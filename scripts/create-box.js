0fab499fef2b7c849df34058cd1234ae1a352325
Gareth Edwards <garrethedwards@gmail.com>
2023-08-14 09:23:57 +0200

    Sepolia deployment

--- scripts/create-box.js
+++ scripts/create-box.js
@@ -1,9 +1,9 @@
 // scripts/create-box.js
 const { ethers, upgrades } = require("hardhat");
 // sepolia
-// const employerBadgeAddress = "0xb7D2887361C90DDf1f1B4630D222C1C9E477ed3d";
+ const employerBadgeAddress = "0x0FC0fd31C2465367047127a87Fda2a565EC0AcA5";
 // base goerli
-const employerBadgeAddress = "0x5c4033d014Fc6b99a2E2858c898608C93e93594E";
+ // const employerBadgeAddress = "0x5c4033d014Fc6b99a2E2858c898608C93e93594E";
 async function main() {
     console.log("Starting...");
     const EmployeeNft = await ethers.getContractFactory("EmployeeNft");
